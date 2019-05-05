c3e44c3ac0bbd7eda74ddb44a02de5a43a77f77b
Philip Bruland <philipbruland@gmail.com>
2019-05-05 18:41:12 +0100

    Introduce police and get them to change the color to black if they spot a violation

--- main.js
+++ main.js
@@ -6,11 +6,13 @@ const height = window.innerHeight;
 
 const step = 10;
 const colorDataSize = 6; // #000000
+const expectedColor = '#888888';
+const crackdownColor = '#000000';
 const colorValueRange = 16; // 0 to F
 const randomRange = 16; // Arbitrary
 
-const displaySizeX = Math.ceil(width / step);
-const displaySizeY = Math.ceil(height / step);
+const displaySizeX = Math.ceil(height / step);
+const displaySizeY = Math.ceil(width / step);
 
 const colorMem = [];
 let blockPointer = 0;
@@ -48,15 +50,35 @@ function initColorMem() {
 
 function getColorForBlock(pointer) {
   let string = '#';
-  for (let i = pointer * colorDataSize, j = 0; j < colorDataSize; i += 1, j += 1) {
+  for (let i = pointer, j = 0; j < colorDataSize; i += 1, j += 1) {
     string += `${colorMem[i]}`;
   }
   return string;
 }
 
+function setColor(pointer, colorString) {
+  let array = colorString.split('');
+  array.shift(); // remove hash
+  for (let i = 0; i < array.length; i += 1) {
+    console.log(parseInt(array[i]));
+    console.log(colorMem[pointer + i]);
+    colorMem[pointer + i] = array[i];
+    console.log(colorMem[pointer + i]);
+  }
+  console.log(getColorForBlock(pointer));
+}
+
+function drawBlock(pointer) {
+  const location = pointer / colorDataSize;
+  const row = Math.floor(location / displaySizeX);
+  const collumn = location - (row * displaySizeX);
+  ctx.fillStyle = getColorForBlock(pointer);
+  ctx.fillRect(row * step, collumn * step, step, step);
+}
+
 function corruptColorMem() {
   const data = [];
-  const pointer = Math.floor(Math.random() * totalBlocks);
+  const pointer = Math.floor(Math.random() * (totalBlocks * colorDataSize));
   const randAmount = Math.floor(Math.random() * randomRange);
 
   for (let i = 0; i < randAmount; i += 1) {
@@ -66,18 +88,31 @@ function corruptColorMem() {
   for (let i = pointer * colorDataSize, j = 0; j < data.length; i += 1, j += 1) {
     colorMem[i] = numToColor[data[j]];
   }
+
+  const blocksToDraw = Math.ceil(randAmount / colorDataSize);
+  const startingPointer = colorDataSize * Math.floor(pointer / colorDataSize)
+  for (let i = 0; i < blocksToDraw; i += 1) {
+    drawBlock(startingPointer + (i * colorDataSize));
+  }
 }
 
-function drawBlock(pointer) {
-  const row = Math.floor(pointer / displaySizeX);
-  const collumn = pointer - (row * displaySizeX);
-  ctx.fillStyle = getColorForBlock(pointer);
-  ctx.fillRect(row * step, collumn * step, step, step);
+function police(pointer, expectation) {
+  let offendingString = getColorForBlock(pointer);
+  console.log(offendingString + '...' + expectation);
+  if (offendingString !== expectation) {
+    if (offendingString === crackdownColor) {
+      setColor(pointer, expectedColor);
+    } else {
+      console.log("CALLED");
+      setColor(pointer, crackdownColor);
+    }
+  }
 }
 
 function drawDisplay() {
+  police(blockPointer, expectedColor);
   drawBlock(blockPointer);
-  if (blockPointer < totalBlocks - 1) blockPointer += 1;
+  if (blockPointer < (totalBlocks * colorDataSize - 1)) blockPointer += colorDataSize;
   else blockPointer = 0;
 }
 
@@ -92,7 +127,7 @@ function init() {
 function game() {
   draw();
   corruptColorMem();
-  setTimeout(game, 5);
+  setTimeout(game, 0);
 }
 
 init();
